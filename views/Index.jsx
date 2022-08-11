a735ce2518b11951d98cebc8bfbb79c4151e6ccc
RayPerez22 <raynyc2285@gmail.com>
2022-08-11 13:05:20 -0400

    set up index.jsx with better style in css

--- views/Index.jsx
+++ views/Index.jsx
@@ -5,32 +5,37 @@ class Index extends React.Component {
         const { Kicks } = this.props
         return (
             <html lang="en">
-            <head>
-                <title>Ray's Sneakers</title>
+            <head>              
                 <link rel="stylesheet" href="/index.css" />
+                <title >Ray's Sneakers</title>
             </head>
-            <body>               
-                <header>Ray's Sneakers</header>                           
-                <div>                   
+            <body className="site">               
+                <header className="mastheader">Ray's Sneakers</header>
+                 <h1  className="page-title">Air Jordans</h1>                       
+                <main className="main-content">                   
                     {Kicks.map((Kicks) => {
                         return(
-                            <div class="site">
+                            <div className='product'>
                                 <div>
-                                <h2>{Kicks.name}</h2> 
+                                <h3 className='sneakername'>{Kicks.name}</h3> 
                                 </div>
                                 <div>
                                     <a href={`/Kicks/${Kicks.id}`}><img src={`${Kicks.img}`}></img></a>
                                 </div>
-                                <div>
+                                <div className='buttons'>
                                     <form action={`/Kicks/${Kicks.id}?_method=DELETE`}method='POST'><button type='submit'>Delete</button></form>
+                                    <a href={`/Kicks/${Kicks.id}/edit`}>Edit</a>
+                                    <button>Add to cart</button>
                                 </div>
                                 <div>
-                                    <a href={`/Kicks/${Kicks.id}/edit`}>Edit</a>
+                                    
                                 </div>
                             </div>
                         )
                     })}                
-                </div>
+                </main>
+                <aside className="sidebar"></aside>
+                <footer className="footer"></footer>
                 <nav>
                     <a href="/Kicks/new">Create a new Sneaker</a>
                 </nav>
